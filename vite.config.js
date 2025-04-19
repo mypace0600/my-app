e86f202dd69f5d41747de33cc955edb298b02d0a
mypace0600 <mypace0600@gmail.com>
2025-04-19 19:22:15 +0900

    dist 관련 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // 빌드 결과물이 dist 폴더에 생성되도록 설정
+  },
 });
