272cb2e6db0a0e5f76f0f78c03fdbb0bd773d0bf
mypace0600 <mypace0600@gmail.com>
2025-05-06 20:10:01 +0900

    splash 내용 추가

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -17,6 +17,14 @@ const Splash = () => {
       <button onClick={handleGoogleLogin} className="login-button">
         Login with Google
       </button>
+      <p>
+        Wordle은 단어 퀴즈를 통해 어휘력을 키우는 게임입니다. 로그인 후 매일
+        새로운 문제를 풀어보세요!
+      </p>
+      <ul>
+        <li>매일 새로운 단어 퀴즈 제공</li>
+        <li>점수 기반 랭킹 시스템</li>
+      </ul>
     </div>
   );
 };
